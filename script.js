4213b9c414468d43110010584c7d1caa5b574b1c
Nsikak owo <owonsikak@gmail.com>
2019-10-16 16:22:38 +0100

    handling delete button with new list items

--- script.js
+++ script.js
@@ -3,43 +3,6 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var listItems=document.getElementsByTagName("li"); 
 
-
-function inputLength() {
-	return input.value.length;
-}
-
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-}
-
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-		var btn=document.createElement("button");
-btn.appendChild(document.createTextNode("Delete!"));
-listItems[i].appendChild(btn);
-btn.onclick=removeParent;
-	}
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-		var btn=document.createElement("button");
-btn.appendChild(document.createTextNode("Delete!"));
-listItems[i].appendChild(btn);
-btn.onclick=removeParent;
-	}
-}
-
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
-
 ul.onclick=function(event){
 	var target=event.target;
 	target.classList.toggle("done");
@@ -67,3 +30,34 @@ deleteButton();
 function removeParent(evt){
 	evt.target.parentNode.remove();
 } 
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+		deleteButton();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+		deleteButton();
+	}
+}
+
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+
